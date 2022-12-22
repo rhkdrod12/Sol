01bbde3e50fd7d05b96ad2e53165c3698eb66583
rhkdrod12 <rhkdrod12@gmail.com>
2022-12-23 00:53:45 +0900

    문제풀이

--- src/LEVEL2/Sol2_2.java
+++ src/LEVEL2/Sol2_2.java
@@ -1,8 +1,7 @@
 package LEVEL2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
 
 public class Sol2_2 {
 	public static void printAnswer(Object val){
@@ -22,7 +21,7 @@ public class Sol2_2 {
 	public static void main(String[] args) {
 		
 		int n = 2;
-		String[] data = {"N~F=0", "R~T>2"};
+		String[] data = {"M~C<2", "M~C<3"};
 		
 		System.out.println();
 		printAnswer(new Solution().solution(n, data));
@@ -54,12 +53,94 @@ public class Sol2_2 {
 			 *
 			 * 재네들을 일단 배치하고 나서 나머지 배치하는 경우의 수를 구하면 된다는 건데..
 			 * 간격이 3이면
-			 *
+			 * a 와 b 의 조건은 여러개일 수 있지..
+			 * boolean의 배열형태로 만들어서 모든 거리 조건을 true false로 처리할까..?
 			 */
 			
+			HashMap<Character, Integer> indexMap = new HashMap<>();
+			for (int i = 0; i < friends.length; i++) {
+				indexMap.put(friends[i], i);
+			}
 			
+			for (String  str : data) {
+				char[] chars = str.toCharArray();
+				boolean b = setCondition(indexMap.get(chars[0]), indexMap.get(chars[2]), chars[4] - 48, chars[3]);
+				if (!b) {
+					return 0;
+				}
+			}
 			
-			return answer;
+			boolean[] visited = new boolean[maxFriends];
+			int[] select = new int[maxFriends];
+			count(0, visited, select);
+			
+			return result;
+		}
+		// "N~F=0", "R~T>2"
+		char[] friends = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+		int result = 0;
+		
+		public void count(int idx,  boolean[] visited, int[] select){
+			if (idx == maxFriends) {
+				result++;
+				return;
+			}
+			for (int i = 0; i < maxFriends; i++) {
+				if (!visited[i]) {
+					visited[i] = true;
+					select[idx] = i;
+					if (ableSeat(select, idx)) {
+						count(idx + 1, visited, select);
+					}
+					visited[i] = false;
+				}
+			}
+		}
+		
+		
+		public boolean ableSeat(int[] select, int idx){
+			for (int i = 0; i < idx; i++) {
+				if (conditions[select[i]][select[idx]][idx - i - 1]) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		// 총 인원은 8명
+		// 거리는 0 ~ 6
+		// condition[a][b][l] a와 b가 l만큼 간격에서 배치될 수 있는지 여부
+		// 기본적으로 전부 배치되는건데 조건으로 인해서 더이상 배치가 안되는거니
+		// false가 배치 가능 true가 배치 불가능으로 판단합시다.
+		int maxFriends = 8;
+		int maxDistance = 7;
+		boolean[][][] conditions = new boolean[maxFriends][maxFriends][maxDistance];
+		
+		/**
+		 *  condtion은 false일 때 갈 수 있고 true일 때 갈 수없으로 판단할 것임
+		 */
+		public boolean setCondition(int a, int b, int distance, char condition) {
+			for (int i = 0; i < maxDistance; i++) {
+				// 조건 중에서 이미갈 수 없는 상황이면
+				if (conditions[a][b][i] || conditions[b][a][i]) {
+					return false;
+				}else{
+					boolean flag = !operation(i, distance, condition);
+					conditions[a][b][i] = flag;
+					conditions[b][a][i] = flag;
+				}
+			}
+			return true;
+		}
+		
+		public boolean operation(int cur, int distance, char condition) {
+			if (condition == '=') {
+				return cur == distance;
+			} else if (condition == '<') {
+				return cur < distance;
+			}else {
+				return cur > distance;
+			}
 		}
 	}
 }
