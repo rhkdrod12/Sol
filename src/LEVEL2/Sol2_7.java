b7c26c301f7796980f073727c521c2ae95f3c0c3
김광진 <rhkdrod12@gmail.com>
2022-12-30 17:52:04 +0900

    문제풀이~

--- src/LEVEL2/Sol2_7.java
+++ src/LEVEL2/Sol2_7.java
@@ -32,24 +32,36 @@ public class Sol2_7 {
 		public int[] solution(int n, int[] info) {
 			int[] answer = {};
 			
+			/**
+			 * 어차피가 모든 점수에서 이긴 경우의 최대 점수
+			 */
 			int appeachCost = 0;
 			for (int i = 0; i < info.length; i++) {
-				appeachCost += info[i] * (10 - i);
+				if (info[i] > 0) {
+					appeachCost += (10 - i);
+				}
 			}
 			
+			// 10점을 따내기 위해 5번이 소요되었고
+			// 09점을 따내기 위해 3번이 소요되었으면
+			//
+			
+			/**
+			 * 10점을 따내기 위해 5번이 소요되었고
+			 * 09점을 따내기 위해 3번이 소요되었으면
+			 *
+			 */
 			int rionCost = 0;
 			boolean[] visited = new boolean[10];
 			PriorityQueue<Node> qu = new PriorityQueue<>();
+			
 			int idx = 0;
 			while (true) {
 				if (info[idx] < n) {
-					int rionCurCnt = info[idx] + 1;
-					int rionCurCost = (10 - idx) * rionCurCnt;
-					
-					n -= rionCurCnt;
-					qu.add(new Node(idx, rionCurCost));
+				
 				}
 			}
+			
 			
 			return answer;
 		}
@@ -57,16 +69,27 @@ public class Sol2_7 {
 		class Node implements Comparable<Node> {
 			
 			int idx;
+			int cnt;
 			int cost;
 			
-			public Node(int idx, int cost) {
+			public Node(int idx, int cnt, int cost) {
 				this.idx = idx;
+				this.cnt = cnt;
 				this.cost = cost;
 			}
 			
 			@Override
 			public int compareTo(Node o) {
 				return o.cost - cost;
+			}
+			
+			@Override
+			public String toString() {
+				return "Node{" +
+				       "idx=" + idx +
+				       ", cnt=" + cnt +
+				       ", cost=" + cost +
+				       '}';
 			}
 		}
 	}
