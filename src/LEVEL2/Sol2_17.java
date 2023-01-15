e01f8abad9c5fad61a2659fa378c3c545ff8979f
rhkdrod12 <rhkdrod12@gmail.com>
2023-01-16 00:26:05 +0900

    문제풀이

--- src/LEVEL2/Sol2_17.java
+++ src/LEVEL2/Sol2_17.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Sol2_17 {
-	public static void printAnswer(Object val){
+	public static void printAnswer(Object val) {
 		if (val instanceof int[]) {
 			System.out.println("val = " + Arrays.toString((int[]) val));
 		} else if (val instanceof long[]) {
@@ -15,7 +15,7 @@ public class Sol2_17 {
 			System.out.println("val = " + Arrays.toString((double[]) val));
 		} else if (val instanceof String[]) {
 			System.out.println("val = " + Arrays.toString((String[]) val));
-		} else{
+		} else {
 			System.out.println("val = " + val);
 		}
 	}
@@ -39,21 +39,9 @@ public class Sol2_17 {
 		public int solution(int n, long l, long r) {
 			int answer = 0;
 			
-			
-			HashMap<Integer, String> convertMap = new HashMap<>();
-			convertMap.put(0, "1");
-			convertMap.put(1, "00000");
-			convertMap.put(2, "11011");
-			
-			
-			HashMap<Integer, String> tableMap = new HashMap<>();
-			tableMap.put(0, "1");
-			
-			tableMap.put(1, "11011");
-			
 			ArrayList<Node2> list = new ArrayList<>();
-			list.add(new Node2(0,"1", 0));
-			list.add(new Node2(1,"11011", 1));
+			list.add(new Node2(0, "1", 0));
+			list.add(new Node2(1, "11011", 1));
 			
 			if (n > 1) {
 				for (int i = 2; i < n; i++) {
@@ -61,82 +49,84 @@ public class Sol2_17 {
 				}
 			}
 			
+			long count = count(list.get(n - 1), l, r);
+			System.out.println("count = " + count);
+			
 			return answer;
 		}
 		
 		public long count(Node2 node2, long l, long r) {
-			long midStat = node2.sideNode.size;
-			long midEnd = midStat + node2.midZeroCnt;
-			long end = node2.size;
-			
 			long answer = 0;
 			
-			// 시작 지점이
-			// 노드 왼쪽에 있는 경우
-			if (midStat < l) {
-				if (midStat < r) {
-					answer+= count(node2.sideNode, l, midStat);
-				} else if(midStat > r){
-					answer+= count(node2.sideNode, l, r);
-				} else if (l == 0) {
-					return node2.oneSize;
+			if (node2.n < 2) {
+				char[] chars = node2.val.toCharArray();
+				for (int i = 0; i < chars.length; i++) {
+					if (i <= l && l <= r && chars[i] == '1') {
+						answer++;
+					}
 				}
-			} else if (midEnd > l) {
-				answer+= count(node2.sideNode, l, r);
+				return answer;
+			}
+			
+			//
+			long stat = 0;
+			long midStat = 2 * node2.sideNode.totalSize;
+			long midEnd = midStat + node2.midZeroCnt;
+			long end = node2.totalSize;
+			
+			 if (l == stat && r == end) {
+				return node2.oneSize;
+			} else if ((l == stat && r == (midStat - 1)) || (l == midEnd && r == end)) {
+				return node2.sideNode.oneSize * 2;
+			}
+			
+			if (l < midStat && r > midEnd) {
+				if (l < node2.sideNode.totalSize) {
+					answer += count(node2.sideNode, l, node2.sideNode.totalSize) + node2.sideNode.oneSize;
+				} else {
+					answer += count(node2.sideNode, l, node2.sideNode.totalSize);
+				}
+				if (r - midEnd < node2.sideNode.totalSize) {
+					answer += count(node2.sideNode, 0, r - midEnd);
+				} else {
+					answer += count(node2.sideNode, 0, r - node2.sideNode.totalSize) + node2.sideNode.oneSize;
+				}
 				
-				answer+= count(node2.sideNode, l - midEnd, midStat);
-			} else{
-				return 0;
+			} else if (l < midStat && r < midStat) {
+				if (l < node2.totalSize && r < node2.totalSize) {
+					answer += count(node2.sideNode, l, r);
+				} else if (l < node2.totalSize && r > node2.totalSize) {
+					answer += count(node2.sideNode, l, node2.sideNode.totalSize) + count(node2.sideNode, 0, r - node2.sideNode.totalSize);
+				} else if (l > node2.totalSize) {
+					answer += count(node2.sideNode, 0, r - l);
+				}
 			}
 			
 			return answer;
 		}
 		
-		class Node2{
+		class Node2 {
 			int n;
-			
-			long midStart;
-			
-			long midEnd;
-			
 			long midZeroCnt;
-			long size;
+			long totalSize;
 			long oneSize;
 			String val;
 			Node2 sideNode;
 			
 			public Node2(int n, String val, int midZeroCnt) {
-				this.n = n;
-				this.val = val;
-				this.size = val.length();
+				this.n          = n;
+				this.val        = val;
+				this.totalSize  = val.length();
 				this.midZeroCnt = midZeroCnt;
-				this.oneSize = this.size - this.midZeroCnt;
-				
+				this.oneSize    = this.totalSize - this.midZeroCnt;
 			}
+			
 			public Node2(int n, Node2 node2) {
-				this.n = n;
-				this.size = (long) Math.pow(5, n);
-				this.midZeroCnt = (long) Math.pow(5, n-1);
-				this.sideNode = node2;
-				this.oneSize = 4 * node2.oneSize;
-			}
-		}
-		
-		class Node{
-			int n;
-			int nodeSize;
-			int oneCnt;
-			String val;
-			List<Node> node = new ArrayList<>();
-			
-			public Node(int n, String val) {
-				this.n = n;
-				this.val = val;
-			}
-			
-			public Node(int n, Node node) {
-				this.n = n;
-				
+				this.n          = n;
+				this.totalSize  = (long) Math.pow(5, n);
+				this.midZeroCnt = (long) Math.pow(5, n - 1);
+				this.sideNode   = node2;
+				this.oneSize    = 4 * node2.oneSize;
 			}
 		}
 	}
