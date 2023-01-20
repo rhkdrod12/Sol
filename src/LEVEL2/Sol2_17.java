a8225202d037a30f7702c1ec1e37247b7eeb94c3
김광진 <rhkdrod12@gmail.com>
2023-01-20 15:18:54 +0900

    문제풀이~

--- src/LEVEL2/Sol2_17.java
+++ src/LEVEL2/Sol2_17.java
@@ -1,9 +1,6 @@
 package LEVEL2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Sol2_17 {
 	public static void printAnswer(Object val) {
@@ -26,9 +23,9 @@ public class Sol2_17 {
 		// int n = 5;
 		// int[] deliveries = {1, 0, 3, 1, 2};
 		// int[] pickups = {0, 3, 0, 4, 0};
-		int n = 4;
-		long l = 4;
-		long r = 17;
+		int n = 20;
+		long l = 1;
+		long r = 1;
 		
 		printAnswer(new Solution().solution(n, l, r));
 		
@@ -37,96 +34,86 @@ public class Sol2_17 {
 	static class Solution {
 		// 택배 배달과 수거하기
 		public int solution(int n, long l, long r) {
-			int answer = 0;
-			
-			ArrayList<Node2> list = new ArrayList<>();
-			list.add(new Node2(0, "1", 0));
-			list.add(new Node2(1, "11011", 1));
-			
+			ArrayList<Node> list = new ArrayList<>();
+			list.add(new Node(0, "1", 1));
+			list.add(new Node(1, "11011", 4));
 			if (n > 1) {
-				for (int i = 2; i < n; i++) {
-					list.add(new Node2(i, list.get(i - 1)));
+				for (int i = 2; i <= n; i++) {
+					list.add(new Node(i, list.get(i - 1)));
 				}
 			}
-			
-			long count = count(list.get(n - 1), l, r);
-			System.out.println("count = " + count);
-			
-			return answer;
+			return (int)count(list.get(n), l - 1, r - 1);
 		}
 		
-		public long count(Node2 node2, long l, long r) {
+		public long count(Node node, long l, long r) {
+			
 			long answer = 0;
 			
-			if (node2.n < 2) {
-				char[] chars = node2.val.toCharArray();
-				for (int i = 0; i < chars.length; i++) {
-					if (i <= l && l <= r && chars[i] == '1') {
+			if (r >= node.totalSize) {
+				r = node.totalSize - 1;
+			}
+			if (l < 0) {
+				l = 0;
+			}
+			
+			if (l >= node.totalSize || r < 0 || l > r) {
+				return 0;
+			} else if (node.n < 2) {
+				char[] chars = node.val.toCharArray();
+				for (int i = (int)l; i <= r; i++) {
+					if (chars[i] == '1') {
 						answer++;
 					}
 				}
 				return answer;
+			} else if (l == node.sLIdx1 && r == node.totalSize - 1) {
+				return node.oneSize;
 			}
 			
-			//
-			long stat = 0;
-			long midStat = 2 * node2.sideNode.totalSize;
-			long midEnd = midStat + node2.midZeroCnt;
-			long end = node2.totalSize;
-			
-			 if (l == stat && r == end) {
-				return node2.oneSize;
-			} else if ((l == stat && r == (midStat - 1)) || (l == midEnd && r == end)) {
-				return node2.sideNode.oneSize * 2;
-			}
-			
-			if (l < midStat && r > midEnd) {
-				if (l < node2.sideNode.totalSize) {
-					answer += count(node2.sideNode, l, node2.sideNode.totalSize) + node2.sideNode.oneSize;
-				} else {
-					answer += count(node2.sideNode, l, node2.sideNode.totalSize);
-				}
-				if (r - midEnd < node2.sideNode.totalSize) {
-					answer += count(node2.sideNode, 0, r - midEnd);
-				} else {
-					answer += count(node2.sideNode, 0, r - node2.sideNode.totalSize) + node2.sideNode.oneSize;
-				}
-				
-			} else if (l < midStat && r < midStat) {
-				if (l < node2.totalSize && r < node2.totalSize) {
-					answer += count(node2.sideNode, l, r);
-				} else if (l < node2.totalSize && r > node2.totalSize) {
-					answer += count(node2.sideNode, l, node2.sideNode.totalSize) + count(node2.sideNode, 0, r - node2.sideNode.totalSize);
-				} else if (l > node2.totalSize) {
-					answer += count(node2.sideNode, 0, r - l);
-				}
-			}
+			// 왼쪽 첫번째 구간
+			answer += this.count(node.sideNode, l, r);
+			// 왼쪽 두번째 구간
+			answer += this.count(node.sideNode, l - node.sLIdx2, r - node.sLIdx2);
+			// 오른쪽 첫번째 구간
+			answer += this.count(node.sideNode, l - node.sRIdx1, r - node.sRIdx1);
+			// 오른쪽 두번째 구간
+			answer += this.count(node.sideNode, l - node.sRIdx1 - node.sRIdx2, r - node.sRIdx1 - node.sRIdx2);
 			
 			return answer;
 		}
 		
-		class Node2 {
+		class Node {
 			int n;
-			long midZeroCnt;
+			// 왼쪽 자식1 노드 시작 위치
+			long sLIdx1 = 0;
+			// 왼쪽 자식2 노드 시작 위치
+			long sLIdx2;
+			// 오른쪽 자식1 노드 시작 위치
+			long sRIdx1;
+			// 오른쪽 자식2 노드 시작 위치
+			long sRIdx2;
+			
 			long totalSize;
 			long oneSize;
 			String val;
-			Node2 sideNode;
+			Node sideNode;
 			
-			public Node2(int n, String val, int midZeroCnt) {
+			public Node(int n, String val, int oneSize) {
 				this.n          = n;
 				this.val        = val;
 				this.totalSize  = val.length();
-				this.midZeroCnt = midZeroCnt;
-				this.oneSize    = this.totalSize - this.midZeroCnt;
+				this.oneSize    = oneSize;
 			}
 			
-			public Node2(int n, Node2 node2) {
+			public Node(int n, Node node) {
 				this.n          = n;
+				this.sLIdx2     = node.totalSize;
+				this.sRIdx1     = node.totalSize * 2 + (long) Math.pow(5, n - 1);
+				this.sRIdx2     = this.sRIdx1 + node.totalSize;
+				
 				this.totalSize  = (long) Math.pow(5, n);
-				this.midZeroCnt = (long) Math.pow(5, n - 1);
-				this.sideNode   = node2;
-				this.oneSize    = 4 * node2.oneSize;
+				this.sideNode   = node;
+				this.oneSize    = 4 * node.oneSize;
 			}
 		}
 	}
